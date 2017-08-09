baeb98a1dd8eb4f670e17fcf96139e383c3f3fb4
Vinicius Mesel <vinicius@bigsp.com.br>
2017-08-09 16:11:51 -0300

    Allowed hosts fixed

--- pyfreelas/settings.py
+++ pyfreelas/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
