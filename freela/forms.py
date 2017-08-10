4c8f257ad127a2de70d917f8f8e768773805e3f8
Vinicius Mesel <vinicius@bigsp.com.br>
2017-08-09 21:28:06 -0300

    Forms added

---
+++ freela/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm
+from freela.models import Freela, Freelancer
+
+
+class FreelaForm(ModelForm):
+    class Meta:
+        model = Freela
+        fields = ["empresa", "email_responsavel_empresa", "link_da_empresa", "titulo_do_job", "descricao", "requisitos", "valor_pago", "tipo_freela"]
+
+
+
+class FreelancerForm(ModelForm):
+    class Meta:
+        model = Freelancer
+        fields = ["nome", "email", "portfolio"]
