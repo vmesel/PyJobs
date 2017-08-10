8999e6ea1df1b95ee7a73d7874c749d107922d00
Vinicius Mesel <vinicius@bigsp.com.br>
2017-08-09 21:59:22 -0300

    Modificado

--- pyfreelas/settings.py
+++ pyfreelas/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = (
 )
 
 RAVEN_CONFIG = {
-            'dsn': os.environ["DSN"],
+            'dsn': os.environ["SENTRY_DSN"],
                 # If you are using git, you can also automatically configure the
                 #     # release based on the git info.
                 #         'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
