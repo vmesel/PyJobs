b97d29e31760b679230e69bff79627dc26057599
Vinicius Mesel <vinicius@bigsp.com.br>
2017-08-12 00:38:37 -0300

    Adicionado módulo de feedback

--- freela/feedback.py
+++ freela/feedback.py
@@ -4,6 +4,7 @@ from freela.models import Freelancer, Freela
 def send_seven_days_email():
     date_last_week = date.today() - timedelta(weeks=1)
     freelancers = Freelancer.objects.filter(data_inscrito__gt=date_last_week)
-    print(freelancers)
+    for f in freelancers:
+        print(f)
 
 send_seven_days_email()
