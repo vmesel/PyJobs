2d68efb8423b7c7e0da61176ff4cfc910e726aad
Vinicius Mesel (@vmesel) <vinicius@bigsp.com.br>
2017-12-02 17:43:25 -0200

    Melhoria na área das empresas e item do menu de doação adicionado

--- pyjobs/views.py
+++ pyjobs/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    print(dir(request.user))
     return render(request, "index.html", context={"user": request.user})
+
+def ads_view(request):
+    return render(request, "ads.html", context={"user": request.user})
