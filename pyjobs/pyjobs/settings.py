97ab4e6bae0640d70e52880e5c033eb0f463c7fe
Ikaro Pinheiro <ikaropinheiro6@gmail.com>
2018-10-04 00:02:54 -0300

    Setting up default secret key

--- pyjobs/pyjobs/settings.py
+++ pyjobs/pyjobs/settings.py
@@ -22,7 +22,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='&2lt&y%plq*dni9y!3*yr-%bneagk77r$og+$sfkvm=z&wtjjb')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
