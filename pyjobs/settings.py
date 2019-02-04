7d96dce2728ef060ff7ea204ffc9047d7676a6c2
Vinicius Mesel <bioinfo.vinicius@gmail.com>
2019-02-04 21:21:31 -0200

    Try to fix the settings.py for the whitenoise

--- pyjobs/settings.py
+++ pyjobs/settings.py
@@ -124,7 +124,7 @@ USE_TZ = False
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'staticfiles'),)
 
 RAVEN_CONFIG = {'dsn': config("SENTRY_DSN", default=None)}
