e3d86ac64e5cbcd9e75fbfbbc27fd08bd046e51d
Eduardo Cuducos <cuducos@gmail.com>
2019-02-05 16:13:05 -0200

    Refactor TelegramPosterTest for readability

--- pyjobs/core/tests/test_utils.py
+++ pyjobs/core/tests/test_utils.py
@@ -1,11 +1,12 @@
-from django.test import TestCase
 from unittest.mock import patch
+
+from django.test import TestCase
 from telegram import TelegramError
 
 from pyjobs.core.utils import post_telegram_channel
 
-class TelegramPosterTest(TestCase):
 
+class TelegramPosterTest(TestCase):
     def setUp(self):
         self.message = "Hello, World!"
 
@@ -13,33 +14,24 @@ class TelegramPosterTest(TestCase):
     @patch('pyjobs.core.utils.Bot')
     def test_post_message_to_telegram_successfully(self, mocked_bot, mocked_config):
         mocked_config.side_effect = ('my-token', 'my-channel')
-        posted_message = post_telegram_channel(self.message)
-        self.assertEqual(posted_message, (True, 'success'))
-
-    @patch('pyjobs.core.utils.config')
-    @patch('pyjobs.core.utils.Bot')
-    def test_post_telegram_channel(self, mocked_bot, mocked_config):
-        mocked_config.side_effect = ('my-token', 'my-channel')
-
-        assert post_telegram_channel(self.message)[0]
-
+        result, message = post_telegram_channel(self.message)
+        self.assertTrue(result)
+        self.assertEqual(message, 'success')
         mocked_config.assert_any_call('TELEGRAM_TOKEN', default=None)
         mocked_config.assert_any_call('TELEGRAM_CHATID', default=None)
-
         mocked_bot.assert_called_with('my-token')
         mocked_bot.return_value.send_message.assert_called_with(
             chat_id='my-channel',
             text=self.message
         )
 
-
     @patch('pyjobs.core.utils.config')
     @patch('pyjobs.core.utils.Bot')
     def test_post_no_auth_telegram_channel(self, mocked_bot, mocked_config):
         mocked_config.side_effect = (None, None)
-
-        assert post_telegram_channel(self.message) == (False, 'missing_auth_keys')
-
+        result, message = post_telegram_channel(self.message)
+        self.assertFalse(result)
+        self.assertEqual(message, 'missing_auth_keys')
 
     @patch('pyjobs.core.utils.config')
     @patch('pyjobs.core.utils.Bot')
