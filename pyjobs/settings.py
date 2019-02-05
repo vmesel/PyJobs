c9fa394a84acb8f6b70ab478dc7d685a0c0563e4
Eduardo Cuducos <cuducos@gmail.com>
2019-02-05 12:21:48 -0200

    Install django-extensions to help in developing

--- pyjobs/settings.py
+++ pyjobs/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django_extensions',
     'pyjobs.core',
     'pyjobs.api',
     'widget_tweaks',
