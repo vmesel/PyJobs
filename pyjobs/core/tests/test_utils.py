640fe5f174479d164e45b35821aae796eda5e258
Eduardo Cuducos <cuducos@gmail.com>
2019-02-05 16:09:28 -0200

    Fix test_post_wrong_auth_telegram_channel

--- pyjobs/core/tests/test_utils.py
+++ pyjobs/core/tests/test_utils.py
@@ -46,12 +46,14 @@ class TelegramPosterTest(TestCase):
     @patch('pyjobs.core.utils.Bot')
     def test_post_wrong_auth_telegram_channel(self, mocked_bot, mocked_config):
         mocked_config.side_effect = ('my-token', 'my-channel')
-        mocked_bot.side_effect = (TelegramError("error"))
-
-        with self.assertRaises(TelegramError):
-            assert post_telegram_channel(self.message) == (False, 'wrong_auth_keys')
-
-            mocked_config.assert_any_call('TELEGRAM_TOKEN', default=None)
-            mocked_config.assert_any_call('TELEGRAM_CHATID', default=None)
-
-            mocked_bot.assert_called_once_with('my-token')
+        mocked_bot.return_value.send_message.side_effect = TelegramError('error')
+        result, message = post_telegram_channel(self.message)
+        self.assertFalse(result)
+        self.assertEqual(message, 'wrong_auth_keys')
+        mocked_config.assert_any_call('TELEGRAM_TOKEN', default=None)
+        mocked_config.assert_any_call('TELEGRAM_CHATID', default=None)
+        mocked_bot.assert_called_once_with('my-token')
+        mocked_bot.return_value.send_message.assert_called_with(
+            chat_id='my-channel',
+            text=self.message
+        )
