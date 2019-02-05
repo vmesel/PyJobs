4ced578cfe299ffb5c48eed3b4d6480665b98a98
Vinicius Mesel <bioinfo.vinicius@gmail.com>
2019-02-05 00:25:54 -0200

    Add tests to newsletter

--- pyjobs/core/utils.py
+++ pyjobs/core/utils.py
@@ -1,25 +1,6 @@
 from decouple import config
-from facebook import GraphAPI
 import telegram
 
-def list_fb_pages():
-    token = config("FB_PYTHON_ACCESSTOKEN", default=None)
-    if token != None:
-        graph = GraphAPI(token)
-        groups = graph.get_object("me/groups")
-        return groups
-    return []
-
-def post_fb_page(message):
-    token = config("FB_PYTHON_ACCESSTOKEN", default=None)
-    if token != None:
-        graph = GraphAPI(token)
-        try:
-            graph.put_object("PyJobs", "feed", message=message)
-        except:
-            pass
-        return True
-    return False
 
 def post_telegram_channel(message):
     telegram_token = config('TELEGRAM_TOKEN', default=None)
