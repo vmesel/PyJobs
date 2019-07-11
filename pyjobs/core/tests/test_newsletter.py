ae9ae18052ec61f9ec0d86ef13eda97d794442d5
Vinicius Mesel <vinicius@bigsp.com.br>
2019-07-11 14:08:36 -0300

    Adds initialy failing test on newsletter

--- pyjobs/core/tests/test_newsletter.py
+++ pyjobs/core/tests/test_newsletter.py
@@ -2,9 +2,11 @@ from unittest.mock import patch
 
 from django.contrib.auth.models import User
 from django.test import TestCase
+from unittest.mock import patch
 
 from pyjobs.core.models import Job, Profile
 from pyjobs.core.newsletter import subscribe_user_to_chimp
+from django.conf import settings
 
 
 class NewsletterTest(TestCase):
@@ -26,3 +28,8 @@ class NewsletterTest(TestCase):
 
     def test_subscribe_to_newsletter(self):
         self.assertFalse(subscribe_user_to_chimp(self.profile))
+
+    # @patch("pyjobs.core.newsletter.MailChimp")
+    # def test_if_called(self, patched_mc):
+    #     sub = subscribe_user_to_chimp(self.profile)
+    #     self.assertTrue(patched_mc.called)
