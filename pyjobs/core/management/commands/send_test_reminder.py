bde6ced4071c3dc045fe3a571e1c6a83c8344b48
Vinicius Mesel <vinicius@bigsp.com.br>
2019-09-28 16:29:37 -0300

    Add test reminder (second chance to user to do it)

---
+++ pyjobs/core/management/commands/send_test_reminder.py
@@ -0,0 +1,49 @@
+import os
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+from django.db import IntegrityError
+from django.template.loader import get_template
+from django.core.mail import EmailMultiAlternatives
+from django.template import Context
+
+from pyjobs.core.models import Job, JobApplication
+from pyjobs.core.email_utils import get_email_with_template
+from django.conf import settings
+from datetime import timedelta
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        job_qs = Job.objects.filter(is_challenging=True)
+        for job in job_qs:
+            job_applications = JobApplication.objects.filter(
+                job=job,
+                challenge_response_at=None,
+                email_sent_at__gte=datetime.now() - timedelta(days=4),
+                email_sent_at__lte=datetime.now() + timedelta(days=3),
+                challenge_resent=False,
+            )
+
+            for job_app in job_applications:
+                print(job_app)
+                person_email_context = {
+                    "vaga": job,
+                    "pessoa": job_app.user.profile,
+                    "mensagem": job_app,
+                }
+                template_person = "job_interest_challenge"
+                person_email_subject = "Reenviando: Teste Técnico da empresa: {}!".format(
+                    job.company_name
+                )
+
+                job_app.challenge_resent = True
+                job_app.save()
+
+                msg_email_person = get_email_with_template(
+                    template_person,
+                    person_email_context,
+                    person_email_subject,
+                    job_app.user.email,
+                )
+                msg_email_person.send()
