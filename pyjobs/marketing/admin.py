d39220137220935564a3fb85c17b305db7c3ea83
Vinicius Mesel <vinicius@bigsp.com.br>
2019-11-04 14:49:56 -0300

    Add sharing models and required testing

--- pyjobs/marketing/admin.py
+++ pyjobs/marketing/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from .models import Contact, Messages, MailingList
+from .models import Contact, Messages, MailingList, Share
 
 
 class MailingListsAdmin(admin.ModelAdmin):
     list_display = ("email", "name", "slug")
 
 
+class SharingAdmin(admin.ModelAdmin):
+    list_display = ("user_sharing", "user_receiving_email", "job")
+
+
+class MessagesAdmin(admin.ModelAdmin):
+    list_display = ("message_title", "message_type")
+
+
 admin.site.register(Contact)
-admin.site.register(Messages)
+admin.site.register(Messages, MessagesAdmin)
 admin.site.register(MailingList, MailingListsAdmin)
+admin.site.register(Share, SharingAdmin)
