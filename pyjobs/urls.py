7dd6aee9b93dac62bdee1272366a452009ca6e61
viniciusban <viniciusban@gmail.com>
2019-11-24 23:18:34 -0300

    Convert auth views to support Django 2.2

--- pyjobs/urls.py
+++ pyjobs/urls.py
@@ -24,27 +24,32 @@ urlpatterns = [
     url("api/", include("pyjobs.api.urls", namespace="api")),
     url(r"", include("pyjobs.core.urls")),
     url(r"", include("pyjobs.marketing.urls")),
-    url(r"^login/$", auth_views.login, {"template_name": "login.html"}, name="login"),
-    url(r"^logout/$", auth_views.logout, {"next_page": "/"}, name="logout"),
+    url(
+        r"^login/$",
+        auth_views.LoginView.as_view(template_name="login.html"),
+        name="login",
+    ),
+    url(r"^logout/$", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
     url(
         r"^password_reset/$",
-        auth_views.password_reset,
-        {"template_name": "pythonistas-area-password-change.html"},
+        auth_views.PasswordResetView.as_view(
+            template_name="pythonistas-area-password-change.html"
+        ),
         name="password_reset",
     ),
     url(
         r"^password_reset/done/$",
-        auth_views.password_reset_done,
+        auth_views.PasswordResetDoneView.as_view(),
         name="password_reset_done",
     ),
     url(
         r"^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
-        auth_views.password_reset_confirm,
+        auth_views.PasswordResetConfirmView.as_view(),
         name="password_reset_confirm",
     ),
     url(
         r"^reset/done/$",
-        auth_views.password_reset_complete,
+        auth_views.PasswordResetCompleteView.as_view(),
         name="password_reset_complete",
     ),
 ]
