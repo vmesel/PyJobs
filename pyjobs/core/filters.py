c5c6cacd94640be33e3141739e62f0475eb92c2e
Vinicius Mesel <vinicius@bigsp.com.br>
2020-03-27 12:32:39 -0300

    Adjust filters for better search results

--- pyjobs/core/filters.py
+++ pyjobs/core/filters.py
@@ -3,13 +3,14 @@ import django_filters
 
 
 class JobFilter(django_filters.FilterSet):
-    state = django_filters.ChoiceFilter(choices=STATE_CHOICES[:-1])
-    salary_range = django_filters.ChoiceFilter(choices=SALARY_RANGES[:-1])
-    job_level = django_filters.ChoiceFilter(choices=JOB_LEVELS[:-1])
     title = django_filters.CharFilter(
         field_name="title", lookup_expr="icontains", label="Título da Vaga"
     )
+    requirements = django_filters.CharFilter(
+        field_name="requirements", lookup_expr="icontains", label="Requisitos"
+    )
+    state = django_filters.ChoiceFilter(choices=STATE_CHOICES[:-1])
+    salary_range = django_filters.ChoiceFilter(choices=SALARY_RANGES[:-1])
+    job_level = django_filters.ChoiceFilter(choices=JOB_LEVELS[:-1])
+    remote = django_filters.BooleanFilter(field_name="remote", label="Remoto?")
 
-    class Meta:
-        model = Job
-        fields = ["requirements", "remote"]
