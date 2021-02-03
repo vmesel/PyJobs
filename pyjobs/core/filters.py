b4dc3382d90721b87cb19b71d081ea9f52e443a9
Vinicius Mesel <vinicius@bigsp.com.br>
2021-02-03 09:38:11 -0300

    Solves issues with translation on the details

--- pyjobs/core/filters.py
+++ pyjobs/core/filters.py
@@ -7,7 +7,7 @@ class JobFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(
         field_name="title", lookup_expr="icontains", label=_("Título da Vaga")
     )
-    state = django_filters.ModelChoiceFilter(queryset=Country.objects.all(), label=_("País"))
+    country = django_filters.ModelChoiceFilter(queryset=Country.objects.all(), label=_("País"))
     contract_form = django_filters.ChoiceFilter(choices=CONTRACT)
     remote = django_filters.BooleanFilter(field_name="remote", label=_("Remoto?"))
     salary_range = django_filters.ChoiceFilter(choices=SALARY_RANGES[:-1])
